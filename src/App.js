796eb58126d3876cb3fc3c032c2124a99e014caa
Madhurjya8 <madhurjya.saha@gmail.com>
2022-02-16 00:12:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Suspense } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+// import NewQuote from "./pages/NewQuote";
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+
+// import QuoteDetail from "./pages/QuoteDetail";
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+
+// import NotFound from "./pages/NotFound";
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+
+// import AllQuotes from "./pages/AllQuotes";
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+
+function App() {
+  return (
+    <Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
+  );
+}
+
+export default App;
